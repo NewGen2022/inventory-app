e26d4b0a1fd88a03022b9956b38f19ebd89cc0b7
NewGen2022 <amirgimari@gmail.com>
2024-08-09 20:56:49 +0300

    added new routers and static files middleware

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import initDB from './db/initDB.js';
+import homePageRouter from './routes/homePage.js';
+import categoriesRouter from './routes/categories.js';
+import addFormRouter from './routes/addForm.js';
 
 // environment config
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-// main app variable
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// main app
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // serve static files from the 'public' directory
+
+// routes
+app.use('/', homePageRouter);
+app.use('/allItems', categoriesRouter);
+app.use('/add', addFormRouter);
 
 // set templating language to ejs
 app.set('view engine', 'ejs');
