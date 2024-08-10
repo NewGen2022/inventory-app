87fe6d205d960d63a5bfdbccff94f2943a1597c0
NewGen2022 <amirgimari@gmail.com>
2024-08-10 22:23:31 +0300

    combined two promises in one, added active page variable

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,25 +1,58 @@
 import {
     getAllItemsQuery,
-    getAllCategoryItemsById,
-    getItemById,
+    getAllCategoryItemsByIdQuery,
+    getItemByIdQuery,
+    getAllCategoriesQuery,
 } from '../db/queries.js';
 
 const getAllItems = async (req, res) => {
-    const allItems = await getAllItemsQuery();
-    res.render('index', { items: allItems });
+    try {
+        const [allItems, allCategories] = await Promise.all([
+            getAllItemsQuery(),
+            getAllCategoriesQuery(),
+        ]);
+
+        res.render('index', {
+            items: allItems,
+            allCategories: allCategories,
+            activePage: 'all-items',
+        });
+    } catch (error) {
+        console.error('Error fetching items or categories:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 const getAllCategoryItems = async (req, res) => {
-    const allCategoryItems = await getAllCategoryItemsById(req.params.id);
+    try {
+        const [allCategoryItems, allCategories] = await Promise.all([
+            getAllCategoryItemsByIdQuery(req.params.id),
+            getAllCategoriesQuery(),
+        ]);
 
-    res.render('index', { items: allCategoryItems });
+        res.render('index', {
+            items: allCategoryItems,
+            allCategories: allCategories,
+            activePage: req.params.id,
+        });
+    } catch (error) {
+        console.error('Error fetching items or categories:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 const getItem = async (req, res) => {
-    const itemById = await getItemById(req.params.id);
-    res.render('index', {
-        items: itemById,
-    });
+    try {
+        const [itemById, allCategories] = await Promise.all([
+            getItemByIdQuery(req.params.id),
+            getAllCategoriesQuery(),
+        ]);
+
+        res.render('index', { items: itemById, allCategories: allCategories });
+    } catch (error) {
+        console.error('Error fetching items or categories:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 export { getAllItems, getAllCategoryItems, getItem };
