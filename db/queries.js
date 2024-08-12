3862b18b89fea762236b81932f5cb9e184a31e58
NewGen2022 <amirgimari@gmail.com>
2024-08-12 21:58:55 +0300

    added query for adding new category

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,6 @@
 import pool from './pool.js';
 
+// Block to get something from database
 const getAllItemsQuery = async () => {
     try {
         const allItems = await pool.query(`
@@ -72,9 +73,26 @@ const getAllCategoriesQuery = async () => {
     return allCategories.rows;
 };
 
+// Block to submit something to database
+const addNewCategoryQuery = async (categoryName) => {
+    try {
+        const query =
+            'INSERT INTO categories(name) VALUES($1) ON CONFLICT (name) DO NOTHING RETURNING id';
+        const values = [categoryName];
+
+        await pool.query(query, values);
+
+        return true;
+    } catch (err) {
+        console.error('Error inserting category:', error);
+        throw error;
+    }
+};
+
 export {
     getAllItemsQuery,
     getAllCategoryItemsByIdQuery,
     getItemByIdQuery,
     getAllCategoriesQuery,
+    addNewCategoryQuery,
 };
