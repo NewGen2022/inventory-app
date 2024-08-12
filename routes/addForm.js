872a093a0b2e3046f71f9baaf3de7c4d41f74a90
NewGen2022 <amirgimari@gmail.com>
2024-08-12 22:04:09 +0300

    changed files name

--- routes/addForm.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addNew } from '../controllers/addFormController.js';
-
-const router = express.Router();
-
-router.get('/:formType', addNew);
-
-export default router;
