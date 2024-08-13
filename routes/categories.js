4d7eeb9432bfe280984a03e395aaf732c23dbdb9
NewGen2022 <amirgimari@gmail.com>
2024-08-13 22:16:34 +0300

    added routes to item delete/edit

--- routes/categories.js
+++ routes/categories.js
@@ -4,11 +4,21 @@ import {
     getAllCategoryItems,
     getItem,
 } from '../controllers/categoriesController.js';
+import { handleItemChanging } from '../controllers/formsController.js';
 
 const router = express.Router();
 
+// route for getting all available items
 router.get('/', getAllItems);
+// route for getting category items by category id
 router.get('/category/:id', getAllCategoryItems);
+// route for getting item by id
 router.get('/item/:id', getItem);
+
+// route for getting item by id for its editing
+router.get('/item/:id/edit', getItem);
+
+// route for item deleting
+router.post('/item/:id', handleItemChanging);
 
 export default router;
