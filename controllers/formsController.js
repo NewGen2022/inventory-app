64aac7b5f13006742b12c75bd25e2ade35982265
NewGen2022 <amirgimari@gmail.com>
2024-08-13 22:14:18 +0300

    added function for handling item delete/edit

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -2,6 +2,7 @@ import {
     getAllCategoriesQuery,
     addNewCategoryQuery,
     deleteCategoryQuery,
+    deleteItemQueryById,
 } from '../db/queries.js';
 
 // rendering new form according to request URL
@@ -37,19 +38,43 @@ const newForm = async (req, res) => {
 
 const handleCategoryAdding = async (req, res) => {
     const newCategory = capitalizeFirstLetter(req.body.newCategoryName);
-    addNewCategoryQuery(newCategory);
+    await addNewCategoryQuery(newCategory);
 
     res.status(200).redirect('/allItems');
 };
 
 const handleCategoryDeleting = async (req, res) => {
     const newCategory = req.body.categoryId;
-    deleteCategoryQuery(newCategory);
+    await deleteCategoryQuery(newCategory);
 
     res.status(200).redirect('/allItems');
 };
 
-export { newForm, handleCategoryAdding, handleCategoryDeleting };
+const handleItemChanging = async (req, res) => {
+    const itemIdToChange = req.params.id;
+    const action = req.body.action;
+
+    if (action === 'delete') {
+        try {
+            await deleteItemQueryById(itemIdToChange);
+            res.status(200).redirect('/allItems');
+        } catch (err) {
+            console.err('Error deleting item: ', err);
+            res.status(500).send('Error deleting item');
+        }
+    } else if (action === 'edit') {
+        res.status(303).redirect(`/allItems/item/${itemIdToChange}/edit`);
+    } else {
+        res.status(400).send('Invalid action');
+    }
+};
+
+export {
+    newForm,
+    handleCategoryAdding,
+    handleCategoryDeleting,
+    handleItemChanging,
+};
 
 const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
