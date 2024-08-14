dfd4845e3c05c76ecae7d1afd33fbd102321721e
NewGen2022 <amirgimari@gmail.com>
2024-08-14 18:49:14 +0300

    added controller to add item, added errors handling

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -1,6 +1,7 @@
 import {
     getAllCategoriesQuery,
     addNewCategoryQuery,
+    addNewItemQuery,
     deleteCategoryQuery,
     deleteItemQueryById,
 } from '../db/queries.js';
@@ -24,6 +25,9 @@ const newForm = async (req, res) => {
     };
 
     const formTemplate = formDetails[formType]?.[action];
+    if (!formTemplate || formTemplate === 'deleteItemForm') {
+        return res.status(404).render('404', { message: 'Page Not Found' });
+    }
     const message = formTemplate
         ? `${capitalizeFirstLetter(action)} ${formType}`
         : 'Page Not Found 404';
@@ -36,13 +40,40 @@ const newForm = async (req, res) => {
     });
 };
 
+// Block controller to add something
 const handleCategoryAdding = async (req, res) => {
     const newCategory = capitalizeFirstLetter(req.body.newCategoryName);
-    await addNewCategoryQuery(newCategory);
+
+    const success = await addNewCategoryQuery(newCategory);
+    if (!success) {
+        return res
+            .status(400)
+            .render('404', { message: 'Failed to add category.' });
+    }
 
     res.status(200).redirect('/allItems');
 };
 
+const handleItemAdding = async (req, res) => {
+    const newItem = {
+        itemName: req.body.itemName,
+        itemCategory: req.body.categoryId,
+        itemDescription: req.body.itemDescription,
+        itemPrice: req.body.itemPrice,
+        itemImageUrl: req.body.itemImageUrl,
+    };
+    const success = await addNewItemQuery(newItem);
+
+    if (!success) {
+        return res
+            .status(400)
+            .render('404', { message: 'Failed to add item.' });
+    }
+
+    res.status(200).redirect('/allItems');
+};
+
+// Block controller to delete something
 const handleCategoryDeleting = async (req, res) => {
     const newCategory = req.body.categoryId;
     await deleteCategoryQuery(newCategory);
@@ -53,6 +84,10 @@ const handleCategoryDeleting = async (req, res) => {
 const handleItemChanging = async (req, res) => {
     const itemIdToChange = req.params.id;
     const action = req.body.action;
+
+    if (!itemIdToChange || isNaN(itemIdToChange)) {
+        return res.status(400).render('404', { message: 'Invalid item ID.' });
+    }
 
     if (action === 'delete') {
         try {
@@ -65,13 +100,14 @@ const handleItemChanging = async (req, res) => {
     } else if (action === 'edit') {
         res.status(303).redirect(`/allItems/item/${itemIdToChange}/edit`);
     } else {
-        res.status(400).send('Invalid action');
+        res.status(400).render('404', { message: 'Invalid action' });
     }
 };
 
 export {
     newForm,
     handleCategoryAdding,
+    handleItemAdding,
     handleCategoryDeleting,
     handleItemChanging,
 };
