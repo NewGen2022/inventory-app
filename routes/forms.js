d189b6e4a49c20b41eccc962965ee0ac0a00bcf3
NewGen2022 <amirgimari@gmail.com>
2024-08-14 21:52:13 +0300

    changed route for form generation, added new route for item editing

--- routes/forms.js
+++ routes/forms.js
@@ -4,17 +4,22 @@ import {
     handleCategoryAdding,
     handleItemAdding,
     handleCategoryDeleting,
+    handleItemEditing,
 } from '../controllers/formsController.js';
 
 const router = express.Router();
 
-// Route to render forms
-router.get('/:formType/:action', newForm);
+// route to render forms
+router.get('/:formType/:action/:id?', newForm);
 
-// Route to handle form submission
+// route for category creating
 router.post('/category/create', handleCategoryAdding);
+// route for category deleting
 router.post('/category/delete', handleCategoryDeleting);
 
+// route for item creating
 router.post('/item/create', handleItemAdding);
+// route for updating item values
+router.post('/item/edit/:id', handleItemEditing);
 
 export default router;
